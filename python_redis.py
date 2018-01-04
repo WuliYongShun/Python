02673b679e52287b1723f28c5da326398c04d24f
robinn00 <robinn00@163.com>
2018-01-04 19:39:52 +0800

    python_redis

--- python_redis.py
+++ python_redis.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 # _*_ coding:utf-8 _*_
 
+import redis
+
 if __name__ == "__main__":
-    print("hello")
+    rs = redis.Redis(host='192.168.0.104', port=6379, db=0, password="robinn")
+    rs.set("addr", "sichuan chengdu")
+    print( rs.get("addr") )
+
+    
