7299ddb79bfe9f2f4ad940cf0e38bac580b0af8b
robinn00 <robinn00@163.com>
2018-01-08 14:42:00 +0800

    mongodb python

---
+++ python_mongodb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# _*_ coding:utf-8 _*_
+
+import pymongo
+
+if __name__ == "__main__":
+    #创建链接
+    mc = pymongo.MongoClient("mongodb://192.168.0.104:27017")
+
+    #获取数据库
+    db = mc.users
+
+    #获取集合
+    col = db.infos
+
+    #插入文档
+    #col.insert({"name": "zhongmama", "age": "23"})
+
+
+    #遍历集合文档
+    for line in col.find():
+        for x in line:
+            print(str(x)+":"+str(line[x]))
+        print("======================")
+
+    #文档个数
+    cnt = col.count()
+    print(cnt)
+
